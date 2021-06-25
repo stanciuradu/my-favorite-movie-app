a6360add5be408a3d2eb225a8ee3105a4ff1e0be
radu <radustanciu841@yahoo.ro>
2021-06-25 17:11:41 +0300

    Introduce react router on pages

---
+++ src/pages/Movie.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Movie extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Pagina de filme</h1>
+            </div>
+        );
+    }
+}
+
+export default Movie;
---
+++ src/pages/Page404.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Page404() {
+    return (
+        <div>
+            <h1>Eroare 404-page not found</h1>
+        </div>
+    );
+}
+
+export default Page404;
